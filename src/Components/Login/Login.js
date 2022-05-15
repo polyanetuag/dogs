c3edc126da7e3877c22db281f9cb7898998e5f91
Polyane Tuag <pollyportugues@hotmail.com>
2022-05-15 09:53:38 -0300

    feat: adding new routes

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom' 
+import LoginForm from './LoginForm'
+import LoginCreate from './LoginCreate'
+import LoginPasswordForgot from './LoginPasswordForgot'
+import LoginPasswordReset from './LoginPasswordReset'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="create" element={<LoginCreate/>} />
+        <Route path="forgot" element={<LoginPasswordForgot />} />
+        <Route path="reset" element={<LoginPasswordReset />} />
+      </Routes>
+    </div>
   )
 }
 
