3d38c0aeb8e7bec75fed9e6e843db794c9fa1c4b
Polyane Tuag <pollyportugues@hotmail.com>
2022-05-16 19:58:20 -0300

    feat: wrapping context in application

--- src/App.js
+++ src/App.js
@@ -5,19 +5,23 @@ import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
+import { UserStorage } from "./UserContext";
 
 function App() {
-  return <div>
-    <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login/*" element={<Login />} />
-
-    </Routes>
-    <Footer />
-    </BrowserRouter>
-  </div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
