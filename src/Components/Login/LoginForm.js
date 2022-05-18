51ac1ffa008ed5005ef856e9d39f61a676ac59e4
Polyane Tuag <pollyportugues@hotmail.com>
2022-05-18 19:28:14 -0300

    feat: incrementing LoginForm page

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
+import Error from "../Helper/Error";
 import { UserContext } from "../../UserContext";
+import styles from "./LoginForm.module.css";
+import stylesBtn from "../Forms/Button.module.css";
 
 const LoginForm = () => {
   const username = useForm();
@@ -20,9 +23,9 @@ const LoginForm = () => {
   }
 
   return (
-    <section>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
+    <section className="animeLeft">
+      <h1 className="title">Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
         {loading ? (
@@ -30,10 +33,16 @@ const LoginForm = () => {
         ) : (
           <Button>Entrar</Button>
         )}
-
-        {error && <p>{error}</p>}
+        <Error error={error} />
       </form>
-      <Link to="/login/create">Cadastro</Link>
+      <Link className={styles.lost} to="/login/lost">
+        Esqueceu a senha?
+      </Link>
+      <div className={styles.register}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui uma conta? Cadastre-se agora.</p>
+      </div>
+      <Link className={stylesBtn.button} to="/login/create">Cadastro</Link>
     </section>
   );
 };
