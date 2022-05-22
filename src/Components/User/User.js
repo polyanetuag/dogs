e1ecc21af76a7cda820cc6ee88f995a6088de31b
Polyane Tuag <pollyportugues@hotmail.com>
2022-05-22 10:09:43 -0300

    feat: User component

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Feed from '../Feed/Feed'
+import UserHeader from './UserHeader'
+import UserPhotoPost from './UserPhotoPost'
+import UserStatistics from './UserStatistics'
 
 const User = () => {
   return (
-    <div>Usuário</div>
+    <section className='container'>
+      <UserHeader />
+      <Routes>
+        <Route path='/' element={<Feed />} />
+        <Route path='post' element={<UserPhotoPost />} />
+        <Route path='statistics' element={<UserStatistics />} />
+      </Routes>
+    </section>
   )
 }
 
-export default User
+export default User 
