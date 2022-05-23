dfceaea5f0330c1604170b3410dab5ba27f9c397
Polyane Tuag <pollyportugues@hotmail.com>
2022-05-23 12:01:00 -0300

    feat: validating data for weight and age

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -5,7 +5,11 @@ const typesValidation = {
         regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         ,
         message: 'Preencha um email válido'
-    }
+    },
+    number: {
+        regex: /^\d+$/,
+        message: 'Preencha apenas com números'
+    },
 }
 
 const useForm = (type) => {
