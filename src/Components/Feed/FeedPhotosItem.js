4c21f5831f4299f413f90d74471448348b0c8912
Polyane Tuag <pollyportugues@hotmail.com>
2022-05-25 20:04:46 -0300

    feat: function to open modal

--- src/Components/Feed/FeedPhotosItem.js
+++ src/Components/Feed/FeedPhotosItem.js
@@ -1,13 +1,17 @@
-import React from 'react'
-import styles from './FeedPhotosItem.module.css'
+import React from "react";
+import styles from "./FeedPhotosItem.module.css";
 
-const FeedPhotosItem = ({photo}) => {
+const FeedPhotosItem = ({ photo, setModalPhoto }) => {
+  function handleClick() {
+    setModalPhoto(photo);
+  }
+
   return (
-    <li className={styles.photo}>
-        <img src={photo.src} alt={photo.title}/>
-        <span className={styles.visualization}>{photo.acessos}</span>
+    <li className={styles.photo} onClick={handleClick}>
+      <img src={photo.src} alt={photo.title} />
+      <span className={styles.visualization}>{photo.acessos}</span>
     </li>
-  )
-}
+  );
+};
 
-export default FeedPhotosItem
+export default FeedPhotosItem;
