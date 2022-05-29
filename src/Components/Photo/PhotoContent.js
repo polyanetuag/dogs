fc7cdddb981a1c0884352d44ba65aa1842de008b
Polyane Tuag <pollyportugues@hotmail.com>
2022-05-29 14:11:27 -0300

    feat: check to delete photo

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../../UserContext";
 import PhotoComments from "./PhotoComments";
 import styles from "./PhotoContent.module.css";
+import PhotoDelete from "./PhotoDelete";
 
 const PhotoContent = ({ data }) => {
+  const user = React.useContext(UserContext);
   const { photo, comments } = data;
 
   return (
@@ -14,14 +17,19 @@ const PhotoContent = ({ data }) => {
       <div className={styles.details}>
         <div>
           <p className={styles.author}>
-            <Link to={`/profile/${photo.author}`}>@{photo.author}</Link>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/profile/${photo.author}`}>@{photo.author}</Link>
+            )}
+
             <span className={styles.visualizations}>{photo.acessos}</span>
           </p>
           <h1 className="title">
             <Link to={`/photo/${photo.id}`}>{photo.title}</Link>
           </h1>
           <ul className={styles.attributes}>
-            <li>{photo.peso} kg</li> 
+            <li>{photo.peso} kg</li>
             <li>{photo.idade} anos </li>
           </ul>
         </div>
@@ -29,6 +37,6 @@ const PhotoContent = ({ data }) => {
       <PhotoComments id={photo.id} comments={comments} />
     </div>
   );
-}; 
+};
 
 export default PhotoContent;
