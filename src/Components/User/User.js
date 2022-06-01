4449180725d05a4154784548d3add916451734aa
Polyane Tuag <pollyportugues@hotmail.com>
2022-06-01 12:28:28 -0300

    feat: setting feed for each user

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,21 +1,24 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Feed from '../Feed/Feed'
-import UserHeader from './UserHeader'
-import UserPhotoPost from './UserPhotoPost'
-import UserStatistics from './UserStatistics'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { UserContext } from "../../UserContext";
+import Feed from "../Feed/Feed";
+import UserHeader from "./UserHeader";
+import UserPhotoPost from "./UserPhotoPost";
+import UserStatistics from "./UserStatistics";
 
 const User = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
-    <section className='container'>
+    <section className="container">
       <UserHeader />
       <Routes>
-        <Route path='/' element={<Feed />} />
-        <Route path='post' element={<UserPhotoPost />} />
-        <Route path='statistics' element={<UserStatistics />} />
+        <Route path="/" element={<Feed user={data?.id} />} />
+        <Route path="post" element={<UserPhotoPost />} />
+        <Route path="statistics" element={<UserStatistics />} />
       </Routes>
     </section>
-  )
-}
+  );
+};
 
-export default User 
+export default User;
