bfc5aec0e241df49cd36d975649d1b0f6d50cc58
Polyane Tuag <pollyportugues@hotmail.com>
2022-06-03 18:21:48 -0300

    feat: profile page

---
+++ src/Components/User/UserProfile.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Feed from '../Feed/Feed'
+import { useParams } from 'react-router-dom'
+
+const UserProfile = () => {
+    const {user} = useParams()
+  return (
+    <section className='container mainSection'>
+        <h1 className='title'>{user}</h1>
+        <Feed />
+    </section>
+  )
+}
+
+export default UserProfile
