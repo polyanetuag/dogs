c30bda4ff7eede8565fdd1c223b0b633825cdffb
Polyane Tuag <pollyportugues@hotmail.com>
2022-06-06 19:35:42 -0300

    fix: fixes for Footer component

--- src/App.js
+++ src/App.js
@@ -13,10 +13,11 @@ import NotFound from "./Components/NotFound";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <BrowserRouter>
         <UserStorage>
           <Header />
+          <main className="AppBody">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="login/*" element={<Login />} />
@@ -25,6 +26,7 @@ function App() {
             <Route path="profile/:user" element={<UserProfile />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
+          </main>
           <Footer />
         </UserStorage>
       </BrowserRouter>
