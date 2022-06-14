db3a683581ba07fe9a43642a3b093ecba894bacb
Polyane Tuag <pollyportugues@hotmail.com>
2022-06-14 19:30:34 -0300

    fix: improving error message

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -35,7 +35,7 @@ const LoginForm = () => {
         ) : (
           <Button>Entrar</Button>
         )}
-        <Error error={error} />
+        <Error error={error && 'Dados incorretos'} />
       </form>
       <Link className={styles.lost} to="/login/forgot">
         Esqueceu a senha?
